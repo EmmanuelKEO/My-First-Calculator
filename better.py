f7fc993407cc1d794f8c83c7d094bd4b1cc010c4
Gargoil <emmanueledusei.k@gmail.com>
2024-04-17 14:14:59 +0000

    Update better.py

--- better.py
+++ better.py
@@ -1,150 +1,156 @@
-def is_valid(_expression):
-    Valid_list = ['/','*','-','+','1','2','3','4','5','6','7','8','9','0','.',' ','(',')']
-    for _item in _expression:
-        if not _item in Valid_list:
+def calculate(expression:str):
+    expression = '+' + expression
+    expression = expression + '+'
+    process:list = []
+    process1:list = []
+    process2:list = []
+    process3:list = []
+    process4:list = []
+    sign:int = 1
+    number:str = ''
+    prevoius_item:str = ''
+    result:float = 0
+    b1:int = 0
+    b2:int = 0
+    catch:bool = False
+    for current_item in expression:
+        if has_valid_alphabets(current_item):
+            while True:
+                print(current_item + '?')
+                response:str = input('>')
+                if has_valid_numbers(response):
+                    if has_valid_numbers(prevoius_item):
+                        process.append('*')
+                    process.append(response)
+                    break
+                else:
+                    print(response, 'is not a number.')
+        else:
+            process.append(current_item)
+        prevoius_item = current_item
+   
+    for current_item in process:
+        if current_item in ['+','-','(',')','*','/','#']:
+            if number == '' and current_item == '(':
+                number = '1.0'
+
+            if not number == '':
+                process1.append(float(number) * sign)
+                number = ''
+                sign = 1
+
+            if current_item in ['(',')','*','/','#']:
+                process1.append(current_item)
+            else:
+                process1.append('#')
+
+        else:
+            number = number + current_item
+
+        if current_item == '+':
+            sign = 1
+
+        if current_item == '-':
+            sign = -1
+    number = ''
+    for current_item in process1:
+        if current_item == '(':
+            b1 = b1 + 1
+        if current_item == ')':
+            b2 = b1
+            b1 = b1 - 1
+        if b2 == 1:
+            catch = False
+            b1 = 0
+            b2 = 0
+        if catch:
+            number = number + str(current_item)
+        else:
+            if number == '':
+                if not current_item in ['(', ')']:
+                    process2.append(current_item)
+            else:
+                process2.append('*')
+                process2.append(calculate(number))
+                number = ''
+        if b1 == 1:
+            catch = True
+    number = ''
+    for current_item in process2:
+        if current_item in ['*', '/', '#']:
+            if not current_item == '/':
+                if number == '':
+                    process3.append(current_item)
+                else:
+                    process3.append(float(number))
+                    process3.append(current_item)
+                number = ''
+        else:
+            if number == '':
+                number = current_item
+            else:
+                number = str(float(number) / float(current_item))   
+    number = ''
+    for current_item in process3:
+        if current_item in ['*', '#']:
+            if not current_item == '*':
+                if number == '':
+                    process4.append(current_item)
+                else:
+                    process4.append(float(number))
+                    process4.append(current_item)
+                number = ''
+        else:
+            if number == '':
+                number = current_item
+            else:
+                number = str(float(number) * float(current_item))
+    number = ''
+    for current_item in process4:
+        if not current_item == '#':
+           result = result + current_item
+    return result
+
+def is_valid(expression:str) -> bool:
+    for item in expression:
+        if not item in ['1','2','3','4','5','6','7','8','9','0','-','+','*','/','.','(',')',' ']:
             return False
     return True
 
-def operate(value1, operator, value2):
-    if operator == '*':
-        return value1 * value2
-    if operator == '/':
-        return value1 / value2
+def has_valid_operators(expression:str) -> bool:
+    for item in expression:
+        if not item in ['/','*','-','+','(',')']:
+            return False
+    return True
 
-def get_list_size(_list):
-    count = 0
-    for x in _list:
-        count += 1
-    return count
-def calculate(_expression, _show):
-    Sign = 1
-    Number = '0'
-    New_expression = []
-    _expression = _expression + '+'
-    for _current_item in _expression:
-        if _current_item in ['/','*','-','+','(',')']:
-            if not Number == '0':
-                New_expression.append(float(Number) * Sign)
-                Number = '0'
-                Sign = 1  
-                if _current_item in ['-','+']:
-                    New_expression.append('#')
+def has_valid_numbers(expression:str) -> bool:
+    for item in expression:
+        if not item in ['1','2','3','4','5','6','7','8','9','0','.']:
+            return False
+    return True
 
-                if _current_item in ['/','*','(',')']:
-                    New_expression.append(_current_item)
-            else:
-                if _current_item == '(':
-                    New_expression.append(1)
-                    Number = '0'
-                    Sign = 1  
-                    if _current_item in ['-','+']:
-                        New_expression.append('#')
+def has_valid_alphabets(expression:str) -> bool:
+    for item in expression:
+        if not item.lower() in ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']:
+            return False
+    return True
 
-                    if _current_item in ['/','*','(',')']:
-                        New_expression.append(_current_item)
-        else:
-            Number = Number + _current_item
-
-        if _current_item == '+':
-            Sign = 1
-
-        if _current_item == '-':
-            Sign = -1
-
-    Result = 0.0
-    Last_number = 0.0
-    Operator = '#'
-    Bracket_stack = []
-    Product_switch = 0
-    Sum = 0.0
-    Product = 0.0
-    Bracket_switch = 0
-    for _current_item in New_expression:
-        if _current_item in ['*','/','(',')','#']:
-            Operator = _current_item
-        if Operator in ['*','/'] and not _current_item in ['*','/']:
-            Product_switch += 1
-            if Product_switch == 1:
-                Sum = Sum - Last_number
-                Product = operate(Last_number,Operator,_current_item)
-            if Product_switch > 1:
-                Product = operate(Product,Operator,_current_item)
-
-            Operator = '#'
-            Last_number = Result
-
-        if not _current_item in ['*','/','(',')','#']:
-            Last_number = _current_item
-            if Operator == '#':
-                if Product_switch == 0:
-                    Sum = Sum + _current_item
-
-        if _current_item == '#':
-            Sum = Sum + Product
-            Product_switch = 0
-            Product = 0.0
-
-        if Operator in ['('] and _current_item in ['(']:
-            Bracket_switch += 1
-            Sum = Sum - Last_number
-            Bracket_stack.append(Last_number)
-            if Bracket_switch == 1:
-                Result = Sum
-                Sum = 0.0
-            Operator = '#'
-            Bracket_switch += 1
-        if Operator in [')'] and _current_item in [')']:
-            Sum = Sum + Product
-            Product = 0.0
-            Product_switch = 0
-            if get_list_size(Bracket_stack) == 1:
-                Result = Result + Sum * Bracket_stack.pop()
-                Sum = Result
-                Result = 0.0
-                Bracket_switch = 0
-            else:
-                Sum = Sum * Bracket_stack.pop()
-            Last_number = Sum
-            Operator = '#'
-
-        
-        if _show:
-            print('expression:',New_expression)
-            print('Result:',Result)
-            print('Last_number:',Last_number)
-            print('Operator:',Operator)
-            print('Bracket_stack:',Bracket_stack)
-            print('Product_switch:',Product_switch)
-            print('Sum:',Sum)
-            print('Product:',Product)
-            print('Bracket_switch:',Bracket_switch)
-            print('_current_item:',_current_item)
-            input('continue?')
-            print(" ")
-            
-        
-    return Sum
-
-def main():
-    print('Calculator')
-    response = ''
-    debug_mode = False
-    current_problem = '3+2(2(2*4+5)(6+8))+5'
+def main() -> None:
+    print('Basic calculator')
+    response:str = ''
     while True:
-        response = input('> ')
-        if response.upper() in ['EXIT','CLOSE','END','QUIT']:
+        response = input('>')
+        if response.upper() in ['HELP','?']:
+            print('This is a basic calculator that supports the following operations: + - / * ()')
+        elif  '=' in response:
+            print('"=" is not supported!')
+        elif response.upper() in ['EXIT','END','QUIT','CLOSE']:
+            input('press enter to quit.')
             break
-        elif response.upper() in ['HELP']:
-            print('Type exit, close, end or quit to close pycalc')
-        elif response.upper() in ['DEBUG']:
-            print('debug mode toggled')
-            debug_mode = (debug_mode + 1) % 2
         elif is_valid(response):
-            print(calculate(response,debug_mode))
-            #print(eval(response))
-            
+            print(calculate(response))
         else:
-            print('Invalid input')
+            print('invalid input')
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
